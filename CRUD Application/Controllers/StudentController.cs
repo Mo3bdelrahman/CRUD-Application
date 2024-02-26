963379780aa0ed3ef698d41ba3e21df993b6832e
Mohamed 3bdelrahman <mohamedbdelrahman2@gmail.com>
2024-02-26 17:55:56 +0200

    Add project files.

---
+++ CRUD Application/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Application.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
